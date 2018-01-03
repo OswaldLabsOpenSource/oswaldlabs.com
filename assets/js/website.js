be73efbc6e6d3382445e43e238e4a363ed433b55
Anand Chowdhary <mail@anandchowdhary.com>
2018-01-03 17:52:38 +0530

    Change footer + accelerator pages

--- assets/js/website.js
+++ assets/js/website.js
@@ -3,6 +3,15 @@ $("[data-toggle='tooltip']").tooltip();
 // For testing only
 $("a").each(function() {
 	if (!$(this).attr("href").includes("http")) {
-		$(this).attr("href", $(this).attr("href") + ".html");
+		if (!$(this).attr("href").includes("?")) {
+			$(this).attr("href", $(this).attr("href") + ".html");
+		} else {
+			$(this).attr("href", $(this).attr("href").split("?")[0] + ".html" + "?" + $(this).attr("href").split("?")[1]);
+		}
 	}
 });
+
+var page = location.href.split("?")[0].replace(".html", "");
+if (page.split("/")[page.split("/").length - 1] === "contact") {
+	$("input[name='department'] option:nth-child(2)").attr("selected", "selected");
+}
