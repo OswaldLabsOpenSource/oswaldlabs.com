56a50f86424031a2797f9fe1461f75eb93fdddaf
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-02 17:11:30 +0200

    Add polyfills for includes()

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,75 @@ const loadJS = (src, cb, ordered) => {
 	return script;
 };
 
+// String.prototype.includes() polyfill
+if (!String.prototype.includes) {
+	String.prototype.includes = function(search, start) {
+	  'use strict';
+	  if (typeof start !== 'number') {
+		start = 0;
+	  }
+  
+	  if (start + search.length > this.length) {
+		return false;
+	  } else {
+		return this.indexOf(search, start) !== -1;
+	  }
+	};
+  }
+  
+// Array.includes polyfill
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+	Object.defineProperty(Array.prototype, 'includes', {
+	  value: function(valueToFind, fromIndex) {
+  
+		if (this == null) {
+		  throw new TypeError('"this" is null or not defined');
+		}
+  
+		// 1. Let O be ? ToObject(this value).
+		var o = Object(this);
+  
+		// 2. Let len be ? ToLength(? Get(O, "length")).
+		var len = o.length >>> 0;
+  
+		// 3. If len is 0, return false.
+		if (len === 0) {
+		  return false;
+		}
+  
+		// 4. Let n be ? ToInteger(fromIndex).
+		//    (If fromIndex is undefined, this step produces the value 0.)
+		var n = fromIndex | 0;
+  
+		// 5. If n ≥ 0, then
+		//  a. Let k be n.
+		// 6. Else n < 0,
+		//  a. Let k be len + n.
+		//  b. If k < 0, let k be 0.
+		var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+  
+		function sameValueZero(x, y) {
+		  return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+		}
+  
+		// 7. Repeat, while k < len
+		while (k < len) {
+		  // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+		  // b. If SameValueZero(valueToFind, elementK) is true, return true.
+		  if (sameValueZero(o[k], valueToFind)) {
+			return true;
+		  }
+		  // c. Increase k by 1. 
+		  k++;
+		}
+  
+		// 8. Return false
+		return false;
+	  }
+	});
+  }
+
 const HELLO_BAR_SHOW = true;
 
 const request = (url, data, callback) => {
@@ -65,19 +134,6 @@ function ready(fn) {
 		});
 	}
 }
-
-// Agastya tracking event listener funciton
-const agastyaTrackLink = event => {
-	const eventElement = event.target || event.toElement || event.srcElement;
-	if (eventElement && window.agastya && typeof window.agastya.secureTrack === "function") {
-		window.agastya.secureTrack({
-			event: "external_link",
-			className: eventElement.className,
-			href: eventElement.getAttribute("href"),
-			innerText: eventElement.innerText
-		});
-	}
-};
 
 window.a11ySettings = window.a11ySettings || {};
 window.a11ySettings.api = true;
@@ -166,7 +222,6 @@ ready(() => {
 			const link = links[i];
 			link.classList.remove("active");
 			link.classList.remove("subactive");
-			link.removeEventListener("click", agastyaTrackLink);
 			if (simplify(link.getAttribute("href")) === simplify(location.pathname)) {
 				link.classList.add("active");
 			} else if (
@@ -176,7 +231,6 @@ ready(() => {
 				link.classList.add("subactive");
 			}
 			if (location.hostname !== link.hostname && !link.querySelectorAll("img").length) {
-				link.addEventListener("click", agastyaTrackLink);
 				link.setAttribute("target", "_blank");
 				link.setAttribute("rel", "noopener noreferrer");
 				if (!link.getAttribute("href")) return;
